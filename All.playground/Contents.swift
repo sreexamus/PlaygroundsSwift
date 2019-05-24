c8d4890c4ab5ab37b783fdebc3a6123a8341b28f
sreexamus <sreestarta@gmail.com>
2019-05-24 16:53:37 -0500

    Closure Capture

--- All.playground/Contents.swift
+++ All.playground/Contents.swift
@@ -1,18 +1,40 @@
 import UIKit
 
-
 struct Add {
     var a: Int
     var b: Int
+
+    var sum: Int {
+        return a + b
+    }
 }
 
-let sum = Add(a: 2, b: 3)
+var sum = Add(a: 2, b: 3)
 
-let closure = {
-    print("sum... \(sum)")
+
+// clousre capture creates a new copy of the object for values types like structs
+let closure = { [sum] in 
+    print("sum... \(sum.sum)")
 }
+
+sum.a = 10
 
 closure()
+
+
+class A {
+  var myClosure: (() -> ())?
+  var contantValue = 23
+  
+  func message() {
+    myClosure = { 
+    self.contantValue = 45
+    }
+  }  
+}
+let a = A()
+a.message()
+// A has strong reference to myClosure. myClosure has strong refernce to A(self strongly captured)
 
 
 
